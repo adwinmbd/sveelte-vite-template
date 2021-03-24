21a3faa433ba800784828772003c119abefbaf27
adwinmbd <adwin.mbidi@outlook.com>
2021-03-24 06:52:12 +0100

    feat: add initial source files

---
+++ README.md
@@ -0,0 +1,55 @@
+# Svelte Vite Template
+
+> Vite starter template for Svelte.
+
+This is a starter [Svelte.js](https://svelte.dev) template bundled with [Vite](https://vitejs.dev).
+
+![Screenshot](svlt-vite.png)
+
+Included features :
+
+- Linting with [Prettier](https://prettier.io/)
+- Module bundling with [Vite](https://vitejs.dev)
+- Deleting directories with [Rimraf](https://github.com/isaacs/rimraf)
+
+## Clone template
+
+In an empty folder run `npx degit adwinmbd/svelte-vite-template#main`
+
+## Install:
+
+```
+npm install
+```
+
+## To run:
+
+```
+npm start
+```
+
+Open browser on [http://localhost:3000](http://localhost:3000) to view the app.
+
+## To build:
+
+```
+npm run build
+```
+
+## To clean:
+
+```
+npm run clean
+```
+
+## To lint code:
+
+```
+npm run lint
+```
+
+## To style code:
+
+```
+npm run format
+```
---
+++ src/App.svelte
@@ -0,0 +1,83 @@
+<!-- App.svelte -->
+<script>
+  /* component logic will go here */
+  let count = 0;
+  function handleClick() {
+    count += 1;
+  }
+</script>
+
+<div class="App">
+  <header class="App-header">
+    <img src="/logo.svg" class="App-logo" alt="logo" />
+    <p>Hello Svelte + Vite !</p>
+    <p>
+      <button on:click={handleClick}>
+        count is: {count}
+      </button>
+    </p>
+    <p>
+      Edit <code>App.svelte</code> and save to test HMR updates.
+    </p>
+    <p>
+      <a
+        class="App-link"
+        href="https://svelte.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Learn Svelte
+      </a>
+      {' | '}
+      <a
+        class="App-link"
+        href="https://vitejs.dev"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Vite Docs
+      </a>
+    </p>
+  </header>
+</div>
+
+<style>
+  /* css will go here */
+  .App {
+    text-align: center;
+  }
+  .App code {
+    background: #0002;
+    padding: 4px 8px;
+    border-radius: 4px;
+  }
+  .App p {
+    margin: 0.4rem;
+  }
+  .App-header {
+    color: #333;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+  }
+  .App-logo {
+    height: 36vmin;
+    pointer-events: none;
+    margin-bottom: 3rem;
+    animation: App-logo-pulse infinite 1.6s ease-in-out alternate;
+  }
+  @keyframes App-logo-pulse {
+    from {
+      transform: scale(1);
+    }
+    to {
+      transform: scale(1.06);
+    }
+  }
+  button {
+    font-size: calc(10px + 2vmin);
+  }
+</style>
